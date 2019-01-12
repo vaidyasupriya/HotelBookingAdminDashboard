a3fa292824d13f1d6bc96e6038fefd89e3157680
ankita <ankita.k@corei.io>
2019-01-12 14:57:48 +0530

    added dynamic data in the user details page

--- src/app/user-profile-detailed/user-profile-detailed.component.ts
+++ src/app/user-profile-detailed/user-profile-detailed.component.ts
@@ -10,29 +10,18 @@ import { HttpClient } from '@angular/common/http';
 export class UserProfileDetailedComponent implements OnInit {
   selectedUserID: any;
   userdetails: any;
-  userID: any;
 
   //
+  userId: any;
   user_FirstName: any;
   user_LastName: any;
-  user_Age: any;
   user_Email: any;
-  user_Id: any;
   user_Phone: any;
-
-  user_BillingAddressLine1: any;
-  user_BillingAddressLine2: any;
-  user_BillingAddressCity: any;
-  user_BillingAddressState: any;
-  user_BillingAddressCountry: any;
-  user_BillingAddressZipcode: any;
-
-  user_ShippingAddressCity: any;
-  user_ShippingAddressCountry: any;
-  user_ShippingAddressLine1: any;
-  user_ShippingAddressLine2: any;
-  user_ShippingAddressState: any;
-  user_ShippingAddressZipcode: any;
+  user_Address: any;
+  user_DateOfBirth: any;
+  user_FacebookId: any;
+  user_GoogleId: any;
+  user_Nationality: any;
   //
   orderDetailes: any;
   constructor(private http: HttpClient, private dynamicapiservice: dynamicApiService) { }
@@ -41,35 +30,29 @@ export class UserProfileDetailedComponent implements OnInit {
 
     this.dynamicapiservice.currentProductASIN.
   subscribe(selectedUserID => this.selectedUserID = selectedUserID);
-  this.http.get('https://signorawareapi.azurewebsites.net/api/users').
+  this.http.get('https://doyenowebapp2.azurewebsites.net/api/users').
   subscribe(data => {
     this.userdetails = data;
-    for (const userdetail of this.userdetails) {
-        if (this.selectedUserID === userdetail.$id) {
-          this.orderDetailes = userdetail.Orders;
-          for (const orderDetail of this.orderDetailes) {
-            // console.log(orderDetail.OrderNumber);
-          }
+    console.log(this.selectedUserID);
 
-          this.userID = userdetail.$id;
-          this.user_Age = userdetail.Age;
-          this.user_BillingAddressCity = userdetail.BillingAddressCity;
-          this.user_BillingAddressCountry = userdetail.BillingAddressCountry;
-          this.user_BillingAddressLine1 = userdetail.BillingAddressLine1;
-          this.user_BillingAddressLine2 = userdetail.BillingAddressLine2;
-          this.user_BillingAddressState = userdetail.BillingAddressState;
-          this.user_BillingAddressZipcode = userdetail.BillingAddressZipcode;
+    for (const userdetail of this.userdetails) {
+        if (this.selectedUserID === userdetail.Id) {
+          // this.orderDetailes = userdetail.Orders;
+          console.log(userdetail);
+
+          // for (const orderDetail of this.orderDetailes) {
+          //   console.log(orderDetail.OrderNumber);
+          // }
+          this.userId = userdetail.Id;
+          this.user_Address = userdetail.Address;
+          this.user_DateOfBirth = userdetail.DateOfBirth;
           this.user_Email = userdetail.Email;
           this.user_FirstName = userdetail.FirstName;
-          this.user_Id = userdetail.Id;
           this.user_LastName = userdetail.LastName;
           this.user_Phone = userdetail.Phone;
-          this.user_ShippingAddressCity = userdetail.ShippingAddressCity;
-          this.user_ShippingAddressCountry = userdetail.ShippingAddressCountry;
-          this.user_ShippingAddressLine1 = userdetail.ShippingAddressLine1;
-          this.user_ShippingAddressLine2 = userdetail.ShippingAddressLine2;
-          this.user_ShippingAddressState = userdetail.ShippingAddressState;
-          this.user_ShippingAddressZipcode = userdetail.ShippingAddressZipcode;
+          this.user_GoogleId = userdetail.GoogleId;
+          this.user_FacebookId = userdetail.FacebookId;
+          this.user_Nationality = userdetail.Nationality;
         }
     }
   });
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -16,37 +16,11 @@ export class UserProfileComponent implements OnInit {
 
 
   users: any;
-  orders: any;
-  totalOrders: any;
-  // isUserTrue = true;
-  // showUserList = true;
-  // showUserForm = false;
-  // orderId: any;
-  // showUserRemovalForm = false;
-  removeUserForm: FormGroup;
-  // foundIdValue: any;
-  // foundFNameValue: any;
-  // foundLNameValue: any;
-
-  // showFindButton = true;
-  // showFindLabel = false;
-  // showdeleteButton = false;
-
 
   constructor( private router: Router, private http: HttpClient, private dynamicapiservice: dynamicApiService) {
    }
 
   ngOnInit() {
-    // this.http.get('https://signorawareapi.azurewebsites.net/api/users').
-    //   subscribe(data => {
-    //   this.users = data;
-    //   for ( const user of this.users) {
-    //      this.totalOrders = Object.keys(user.Orders).length;
-    //      console.log(user.Orders);
-    //    }
-    //   },
-    //     err => { console.log(err); }
-    //   );
           this.http.get('https://doyenowebapp2.azurewebsites.net/api/users').
           subscribe(data => {
             this.users = data;
@@ -56,10 +30,6 @@ export class UserProfileComponent implements OnInit {
           }, error => {
             console.log(error);
           });
-
-      // this.removeUserForm = new FormGroup({
-      //   'userId': new FormControl(null, [Validators.required]),
-      // });
   }
 
 
@@ -69,100 +39,4 @@ export class UserProfileComponent implements OnInit {
     this.dynamicapiservice.setSelectedUser(user_id);
   }
 
-  // expand_UserTable() {
-  //   this.isUserTrue = !this.isUserTrue;
-  //   this.showUserList = !this.showUserList;
-  // }
-
-  // openAddUser() {
-  //   this.showUserForm = !this.showUserForm;
-  //   window.scrollTo( 0 , 0 );
-
-  // }
-  // newUserSubmit() {
-  //   alert('User data is submitted');
-
-  // }
-  // openRemoveUser() {
-  //   this.showUserRemovalForm = !this.showUserRemovalForm;
-  // }
-  // userFindToRemove() {
-  // //  console.log(this.removeProductForm.value.asin);
-  //     this.http.get('https://signorawareapi.azurewebsites.net/api/users').
-  //     subscribe(data => {
-  //       this.users = data;
-  //       console.log(this.removeUserForm);
-  //        const totalLength = Object.keys(this.users).length;
-  //        console.log(this.users);
-  //         for (const user of this.users) {
-  //          if (this.removeUserForm.value.userId === user.$id) {
-  //             this.foundIdValue = user.Id;
-  //             console.log(this.removeUserForm.value.userId);
-  //             this.foundFNameValue = user.FirstName;
-  //             this.foundLNameValue = user.LastName;
-  //             this.showFindButton = false;
-  //             this.showFindLabel = true;
-  //             this.showdeleteButton = true;
-  //             console.log(this.foundIdValue);
-
-  //             for (const order of user.Orders) {
-  //               console.log(order.Id);
-  //               this.orderId = order.Id;
-  //              }
-  //          }
-  //         }
-  //       },
-  //       err => {
-  //         console.log(err);
-  //       }
-  //     );
-  // }
-
-  // userRemoval(from, align) {
-  //   this.http.delete('https://signorawareapi.azurewebsites.net/api/users/' + this.foundIdValue).
-  //   subscribe(data => {
-  //   this.users = data;
-  //   window.location.reload();
-  //   },
-  //     err => { console.log(err); }
-  //   );
-
-
-
-  //   if ( this.foundIdValue === this.temp ) {
-  //     this.http.get('https://signorawareapi.azurewebsites.net/api/orders' ).
-  //     subscribe(data => {
-  //     this.orders = data;
-  //     for ( const order of this.orders) {
-  //       // window.location.reload();
-
-  //     }
-  //     },
-  //       err => { console.log(err); }
-  //     );
-  //   }
-
-
-  //   // this.http.delete('https://signorawareapi.azurewebsites.net/api/orders/').
-  //   // subscribe(data => {
-  //   // this.orders = data;
-  //   // for (  const order of this.orders) {
-  //   //   console.log(order);
-  //   // }
-  //   // window.location.reload();
-  //   // },
-  //   //   err => { console.log(err); }
-  //   // );
-  //   //   $.notify({
-  //   //       icon: 'notifications',
-  //   //       message: this.foundFNameValue + ' ' + this.foundLNameValue + 'is Deleted from records!'
-  //   //   }, {
-  //   //       type: 'warning',
-  //   //       timer: 4000,
-  //   //       placement: {
-  //   //           from: from,
-  //   //           align: align
-  //   //       },
-  //   //   });
-  // }
 }
